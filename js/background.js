58a8e2b6c7f273e24332b7e50ca3e5b1a6da4d33
Bestie <lucientown@outlook.com>
2016-11-15 14:51:13 +0800

    完善同步规则

--- js/background.js
+++ js/background.js
@@ -1,11 +1,13 @@
 var playingId;
+var playingType;
 chrome.runtime.onConnect.addListener(function(port){
     if (port.name == 'syncMusic') {
         port.onMessage.addListener(function(msg){
             if (!!msg.playingId) {
                 playingId = msg.playingId;
-                console.log('Recieved Playing ID : '+ msg.playingId);
-                port.postMessage({status: 'recieved',playingId: msg.playingId});
+                playingType = msg.playingType;
+                console.log('Recieved Playing ID : ' + playingId + ' Type :' + playingType);
+                port.postMessage({status: 'recieved', playingId: playingId, playingType: playingType});
             }
         });
     }
--- js/sync_music.js
+++ js/sync_music.js
@@ -16,28 +16,32 @@ function playOnClient(id, type) {       //id为资源id，type为资源类型，
     playArea.click();
 }
 
-function sendMusicId(port,playingId) {
-    port.postMessage({playingId: playingId});
+function sendMusicId(port,playingId,playingType) {
+    port.postMessage({playingId: playingId, playingType: playingType});
     port.onMessage.addListener(function (msg) {
         if (msg.status == 'recieved') {
-            console.log('Send success! Playing ID : ' +msg.playingId);
+            console.log('Send success! Playing ID : ' + msg.playingId + ' Type :' + msg.playingType);
         }
     });
 }
 
 (function listenDOMChange() {
-    var playerStatus = $('#g_player .ply.j-flag')[0];
+    var playerStatus = document.querySelector('.ply.j-flag');
     var playingId;
+    var playingType;
     var config = { attributes: true };
     var port = chrome.runtime.connect({name: "syncMusic"});
 
     var statusObserver = new MutationObserver(function (mutations) {
-        //console.log(mutations.length);
+        //console.log(typeof mutations[mutations.length-1].target);
         //console.log('Status change to '+mutations[mutations.length-1].target.getAttribute('data-action'));
         if (mutations[mutations.length-1].target.getAttribute('data-action') == 'pause') {
-            playingId = $('#g_player .f-thide.name.fc1.f-fl')[0].getAttribute('href').substr(9);
-            console.log(playingId);
-            sendMusicId(port,playingId);
+            var href = document.querySelector('#g_player .f-thide').getAttribute('href');
+            playingType = href.search('song')!=-1?18:17;        //匹配播放类型
+            //console.log(playingType);
+            playingId = href.match(/[1-9]\d*/g)[0];        //匹配id
+            //console.log(playingId);
+            sendMusicId(port, playingId, playingType);
             statusObserver.disconnect();
             setTimeout(function() {statusObserver.observe(playerStatus,config);},3000); //如果观察到pause，则停止观察3秒再重新观察，以避免重复
         }
